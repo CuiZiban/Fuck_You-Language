395df279288bf21135d8a8038375ce5438c11832
CuiZiban <CuiZiban@outlook.com>
2025-11-28 22:50:31 +0800

    Update FUCK_YOU_LANGUAGE解释器.cpp

--- FUCK_YOU_LANGUAGE解释器.cpp
+++ FUCK_YOU_LANGUAGE解释器.cpp
@@ -1,14 +1,12 @@
 #include<bits/stdc++.h>
+#define int long long
 using namespace std;
 map<string,int>bl;
 map<string, vector<int> > lists;
-map<string,pair<long long,string> > names;
 struct num_to_do{bool type;string a;int b;}nums[10000][2000];
-long long ttime=1763124500;
 string s[1000000];
-bool is_in,is_daoru,a[1000][2000];
+bool a[1000][2000];
 int i2[10000],beg=0,nownum[10000],cntcode=0;
-
 void donum(string x,int nowif)
 {
 	int ok=1,ret=0;
@@ -24,7 +22,20 @@ void donum(string x,int nowif)
 	if(ok==0)nums[nowif][nownum[nowif]].type=0,nums[nowif][nownum[nowif]].a=x,nownum[nowif]++;
 	if(ok==1)nums[nowif][nownum[nowif]].type=1,nums[nowif][nownum[nowif]].b=ret,nownum[nowif]++;
 }
-void error(){cout<<"error!you fucked YOU by a wrong way!\n";while(1);}
+void error(int nowif)
+{
+	cout<<"error!you fucked yourself by a wrong way!\nfrom ";
+	if(a[nowif][0]==1)cout<<"in ";
+	if(a[nowif][1]==1)cout<<"out ";
+	if(a[nowif][2]==1)cout<<"defnum ";
+	if(a[nowif][3]==1)cout<<"changenum ";
+	if(a[nowif][4]==1)cout<<"deflist ";
+	if(a[nowif][5]==1)cout<<"changelist ";
+	if(a[nowif][6]==1)cout<<"listtonum ";
+	if(a[nowif][7]==1)cout<<"if ";
+	if(a[nowif][8]==1)cout<<"for ";
+	while(1);
+}
 void dosth(string x,int nowif)
 {
 	if(x=="in")a[nowif][0]=1;
@@ -47,15 +58,15 @@ void dosth(string x,int nowif)
 				break;
 			}
 		}
-		if(ok==0)error();
+		if(ok==0)error(nowif);
 		donum(s[i2[nowif-1]],nowif);
 	}
 	if(x=="end")
 	{
 		if(a[nowif][0]==1)
 		{
-			if(nums[nowif][0].type==1)error();
-			if(nums[nowif][1].type==1)error();
+			if(nums[nowif][0].type==1)error(nowif);
+			if(nums[nowif][1].type==1)error(nowif);
 			if(nums[nowif][0].a=="cnm")
 			{
 				char x;
@@ -68,42 +79,42 @@ void dosth(string x,int nowif)
 				cin>>x;
 				bl[nums[nowif][1].a]=x;
 			}
-			else error();
+			else error(nowif);
 		}
 		else if(a[nowif][1]==1)
 		{
-			if(nums[nowif][0].type==1)error();
+			if(nums[nowif][0].type==1)error(nowif);
 			if(nums[nowif][1].type==0)nums[nowif][1].b=bl[nums[nowif][1].a];
 			if(nums[nowif][0].a=="cnm")cout<<((char) nums[nowif][1].b);
 			else if(nums[nowif][0].a=="CNM")cout<<nums[nowif][1].b;
-			else error();
+			else error(nowif);
 		}
 		else if(a[nowif][2]==1)
 		{
-			if(nums[nowif][0].type==1)error();
+			if(nums[nowif][0].type==1)error(nowif);
 			if(nums[nowif][1].type==0)bl[nums[nowif][0].a]=bl[nums[nowif][1].a];
 			else bl[nums[nowif][0].a]=nums[nowif][1].b;
 		}
 		else if(a[nowif][3]==1)
 		{
-			if(nums[nowif][0].type==1)error();
-			if(nums[nowif][1].type==1)error();
+			if(nums[nowif][0].type==1)error(nowif);
+			if(nums[nowif][1].type==1)error(nowif);
 			if(nums[nowif][2].type==0)nums[nowif][2].b=bl[nums[nowif][2].a];
 			if(nums[nowif][1].a=="Fuck")bl[nums[nowif][0].a]+=nums[nowif][2].b;
 			if(nums[nowif][1].a=="fUck")bl[nums[nowif][0].a]-=nums[nowif][2].b;
 			if(nums[nowif][1].a=="fuCk")bl[nums[nowif][0].a]*=nums[nowif][2].b;
-			if(nums[nowif][1].a=="fuck")bl[nums[nowif][0].a]/=nums[nowif][2].b;
+			if(nums[nowif][1].a=="fucK")bl[nums[nowif][0].a]/=nums[nowif][2].b;
 			if(nums[nowif][1].a=="FUck")bl[nums[nowif][0].a]=(bl[nums[nowif][0].a]>nums[nowif][2].b);
 			if(nums[nowif][1].a=="fuCK")bl[nums[nowif][0].a]=(bl[nums[nowif][0].a]==nums[nowif][2].b);
 		}
 		else if(a[nowif][4]==1)
 		{
-			if(nums[nowif][0].type==1)error();
+			if(nums[nowif][0].type==1)error(nowif);
 			lists[nums[nowif][0].a] = vector<int>(100000, 0);
 		}
 		else if(a[nowif][5]==1)
 		{
-		    if(nums[nowif][0].type==1)error();
+		    if(nums[nowif][0].type==1)error(nowif);
 		    if(nums[nowif][1].type==0)nums[nowif][1].b=bl[nums[nowif][1].a];
 		    if(nums[nowif][2].type==0)nums[nowif][2].b=bl[nums[nowif][2].a];
 		    
@@ -111,66 +122,68 @@ void dosth(string x,int nowif)
 		    if(nums[nowif][1].b >= 0 && nums[nowif][1].b < vec.size()) {
 		        vec[nums[nowif][1].b] = nums[nowif][2].b;
 		    } else {
-		        error();
+		        error(nowif);
 		    }
 		}
 		else if(a[nowif][6]==1)
 		{
-		    if(nums[nowif][0].type==1)error();
-		    if(nums[nowif][1].type==1)error();
+		    if(nums[nowif][0].type==1)error(nowif);
+		    if(nums[nowif][1].type==1)error(nowif);
 		    if(nums[nowif][2].type==0)nums[nowif][2].b=bl[nums[nowif][2].a];
 		    
 		    vector<int>& vec = lists[nums[nowif][1].a];
 		    if(nums[nowif][2].b >= 0 && nums[nowif][2].b < vec.size()) {
 		        bl[nums[nowif][0].a] = vec[nums[nowif][2].b];
 		    } else {
-		        error();
+		        error(nowif);
 		    }
 		}
 		else if(a[nowif][7]==1)
 		{
 			if(nums[nowif][0].type==0)nums[nowif][0].b=bl[nums[nowif][0].a];
-			if(nums[nowif][1].type==0)error();
-			if(nums[nowif][2].type==0)error();	
-			if(nums[nowif][0].b==0)return;
-			int cntend=0; 
-			for(i2[nowif]=0;cntend<nums[nowif][1].b-1&&i2[nowif]<cntcode-1;i2[nowif]++)
+			if(nums[nowif][1].type==0)error(nowif);
+			if(nums[nowif][2].type==0)error(nowif);
+			if(nums[nowif][0].b!=0)
 			{
-				if(s[i2[nowif]]=="YOU!")cntend++;
-			}
-			for(;cntend<nums[nowif][2].b&&i2[nowif]<cntcode-1;i2[nowif]++)
-			{
-				if(s[i2[nowif]]=="YOU!")
+				int cntend=0;
+				for(i2[nowif]=0;cntend<nums[nowif][1].b-1&&i2[nowif]<cntcode;i2[nowif]++)
 				{
-					dosth("end",nowif+1);
-					cntend++;
-					continue;
+					if(s[i2[nowif]]=="YOU!")cntend++;
 				}
-				else if(s[i2[nowif]]=="FUCK")dosth("in",nowif+1);
-				else if(s[i2[nowif]]=="fuck")dosth("out",nowif+1);
-				else if(s[i2[nowif]]=="FuCk")dosth("defnum",nowif+1);
-				else if(s[i2[nowif]]=="FUcK")dosth("deflist",nowif+1);
-				else if(s[i2[nowif]]=="fUcK")dosth("changenum",nowif+1);
-				else if(s[i2[nowif]]=="fUCK")dosth("changelist",nowif+1);
-				else if(s[i2[nowif]]=="FUCk")dosth("listtonum",nowif+1);
-				else if(s[i2[nowif]]=="FucK")dosth("if",nowif+1);
-				else if(s[i2[nowif]]=="fUCk")dosth("for",nowif+1);
-				else dosth("num",nowif+1);
-			}			
+				for(;cntend<nums[nowif][2].b&&i2[nowif]<cntcode;i2[nowif]++)
+				{
+					if(s[i2[nowif]]=="YOU!")
+					{
+						dosth("end",nowif+1);
+						cntend++;
+						continue;
+					}
+					else if(s[i2[nowif]]=="FUCK")dosth("in",nowif+1);
+					else if(s[i2[nowif]]=="fuck")dosth("out",nowif+1);
+					else if(s[i2[nowif]]=="FuCk")dosth("defnum",nowif+1);
+					else if(s[i2[nowif]]=="FUcK")dosth("deflist",nowif+1);
+					else if(s[i2[nowif]]=="fUcK")dosth("changenum",nowif+1);
+					else if(s[i2[nowif]]=="fUCK")dosth("changelist",nowif+1);
+					else if(s[i2[nowif]]=="FUCk")dosth("listtonum",nowif+1);
+					else if(s[i2[nowif]]=="FucK")dosth("if",nowif+1);
+					else if(s[i2[nowif]]=="fUCk")dosth("for",nowif+1);
+					else dosth("num",nowif+1);
+				}	
+			}
 		}
 		else if(a[nowif][8]==1)
 		{
-			if(nums[nowif][1].type==0)error();
-			if(nums[nowif][2].type==0)error();	
+			if(nums[nowif][1].type==0)error(nowif);
+			if(nums[nowif][2].type==0)error(nowif);	
 			if(nums[nowif][0].type==0)nums[nowif][0].b=bl[nums[nowif][0].a];
 			while(nums[nowif][0].b!=0)
 			{
 				int cntend=0;
-				for(i2[nowif]=0;cntend<nums[nowif][1].b-1&&i2[nowif]<cntcode-1;i2[nowif]++)
+				for(i2[nowif]=0;cntend<nums[nowif][1].b-1&&i2[nowif]<cntcode;i2[nowif]++)
 				{
 					if(s[i2[nowif]]=="YOU!")cntend++;
 				}
-				for(;cntend<nums[nowif][2].b&&i2[nowif]<cntcode-1;i2[nowif]++)
+				for(;cntend<nums[nowif][2].b&&i2[nowif]<cntcode;i2[nowif]++)
 				{
 					if(s[i2[nowif]]=="YOU!")
 					{
@@ -196,41 +209,8 @@ void dosth(string x,int nowif)
 		nownum[nowif]=0;		
 	}
 }
-int main()
+signed main()
 {
-	names["cuizihangya"]={1e9,"12345678"};
-	names["flat_fat_cat"]={1e9,"ffckcuf"};
-	cout<<"是否登录？\n";
-	cout<<"是 1，否 0\n";
-	cout<<"登录可享代码保存权及运行已保存代码权利\n";
-	int temp;cin>>temp;
-	if(temp==1)
-	{
-		cout<<"输入账号密码：账号 密码\n"; 
-		string x,y;
-		while(1)
-		{
-			cin>>x>>y;
-			if(names.count(x))
-			{
-				if(names[x].second==y)
-				{
-					if((time(0)-ttime)/60/60/24>names[x].first)
-					{
-						cout<<"VIP已过期，请联系@cuizihang\n";
-						return 0;
-					}
-					break;
-				}
-			}
-			cout<<"错误，请重新输入\n";
-		}
-		cout<<"登录成功，是否运行\n是 1，否 0\n";
-		cin>>temp;
-		if(temp==1)is_daoru=1,freopen("code.txt","r",stdin);
-		is_in=1;
-	}
-	if(is_daoru==0)cout<<"开始输入代码：\n";
 	string p;
 	while(1)
 	{
@@ -238,8 +218,6 @@ int main()
 		cntcode++;
 		if(s[cntcode-1]=="I_HAD_FUCK_YOU!")break;
 	}
-	if(is_daoru==1)cout<<"导入完毕，请开始输入\n",freopen("CON","r",stdin);
-	else cout<<"已写入，请开始输入\n";
 	for(i2[0]=0;i2[0]<cntcode-1;i2[0]++)
 	{
 		if(s[i2[0]]=="I_START_TO_FUCK_YOU!")
@@ -260,17 +238,4 @@ int main()
 		else if(s[i2[0]]=="YOU!")dosth("end",1);
 		else dosth("num",1);
 	}
-	if(is_in&&!is_daoru)
-	{
-		cout<<"\n是否保存代码?\n是 1，否 0\n";
-		cin>>temp;
-		if(temp==1)
-		{
-			freopen("code.txt","w",stdout);
-			for(int i=0;i<cntcode;i++)cout<<s[i]<<" ";
-		}
-		freopen("CON","w",stdout);
-		cout<<"已保存\n"; 
-	}
-	while(1);
 }
